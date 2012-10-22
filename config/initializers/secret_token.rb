dfc4c0743c6036bbc781e867062f458298b5cb0b
petekinnecom <pkinnecom@elctech.com>
2012-10-22 13:10:21 -0700

    heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petstorish::Application.config.secret_token = '590f2d58abb2b9b678a030a877f62c47d5156813e6e30c281a13b47ed4d6574e3db0fd87fe781bd543b9e8e394b4a87e93b4f3c645d0c3ddff7ad2aead13773e'
