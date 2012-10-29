a4de39310f398da4d340ea5301086b077447fc11
Matthew Danskey <mdanskey@gmail.com>
2012-10-29 11:51:20 -0700

    Capybara product_edit tests

--- spec/integration/product_spec.rb
+++ spec/integration/product_spec.rb
@@ -32,3 +32,32 @@ describe "Adding a product" do
   end
 
 end
+
+describe "edit a product" do
+  context "rosy path" do
+    it "makes some edits" do
+      visit '/products/1/edit'
+      fill_in 'product_name', :with => 'Steve-o-meter'
+      fill_in 'product_description', :with => 'Measures the steves'
+      fill_in 'product_price', :with => 131.00
+      select('pets', :from => 'product_category')  
+      attach_file('product_image', File.expand_path('test/images/img.jpg'))
+      click_button 'Save'
+      current_path.should match('/products/1')
+      page.should have_content('Steve-o-meter')
+    end
+  end
+
+  context "painful path" do
+    it "tries to save bad data" do
+      visit '/products/1/edit'
+      fill_in 'product_name', :with => ''
+      fill_in 'product_description', :with => 'Measures the steves'
+      fill_in 'product_price', :with => '' 
+      select('pets', :from => 'product_category')  
+      attach_file('product_image', File.expand_path('test/images/img.jpg'))
+      click_button 'Save'
+      page.should have_content('errors')
+    end 
+  end
+end
--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Option do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validates fields"
+    before(:each) do
+      @valid_attributes = {
+        :family => "color",
+        :value => "aquamarine",
+        :product_id => 19
+      }
+    @option = Option.create
+    end
+
+    it 'should be valid with all options' do
+      @option.attributes = @valid_attributes
+      @option.should be_valid
+    end
+
+    it 'requires a family' do
+      @option.attributes = @valid_attributes.except(:family)
+      @option.should_not be_valid
+    end
+    it 'requires a value' do
+      @option.attributes = @valid_attributes.except(:value)
+      @option.should_not be_valid
+    end
+    it 'requires a product_id' do
+      @option.attributes = @valid_attributes.except(:product_id)
+      @option.should_not be_valid
+    end
+
 end
