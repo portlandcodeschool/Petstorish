3615219c456193b85bb5b7d88fc31e2df97e1974
Matthew Danskey <mdanskey@gmail.com>
2012-11-01 10:57:19 -0700

    Add styling, thumbnails; revert #optSubmit's href
    a#optSubmit, with an href of '#', puts the user at the
    top of the page if clicked. This makes the ajax it drives
    much less slick. It is clickable with Capybara without an
    href if you use find('#optSubmit')

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates_presence_of :name, :description, :price, :category
   attr_accessible :name, :description, :price, :image, :category, :option_assignments
-  has_attached_file :image
+  has_attached_file :image, :styles => {thumbnail: "100x100>", display: "500x500>"}
 
   has_many :option_assignments
   has_many :options, :through => :option_assignments
